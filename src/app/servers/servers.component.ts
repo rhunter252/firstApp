f213fc5be82ff0059d939c90d5222805e88746c6
Randy Hunter <randyh252@gmail.com>
2023-06-09 14:07:29 -0400

    data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // 3 ways to for selectors to be written
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  template: `<app-server></app-server><app-server></app-server>`,
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {}
